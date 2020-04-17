2c6512d00c28ceb8a716315f9bcad007b10e096e
rafsousa <rodrigo.alex.sousa@gmail.com>
2020-04-16 23:40:09 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import modelEntities.Departament;
 import modelEntities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 		Departament obj = new Departament(1, "Books");
 		
 		Seller seller = new Seller(21, "Ana", "ana@gmail.com", new Date(), 3000.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 		
