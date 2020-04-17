0eafcc5f4e38b583ff369ef06cc36202c9572d34
rafsousa <rodrigo.alex.sousa@gmail.com>
2020-04-17 20:42:21 -0300

    Delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,6 +14,7 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== Teste 1 : seller findById ===");
@@ -36,6 +38,29 @@ public class Program {
 		Seller newSeller = new Seller(null, "Bob", "bob@gmail.com", new Date(), 4500.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inseriu novo seller. ID : " + newSeller.getId());
+		
+		System.out.println("\n=== Teste 5 : seller update ===");
+		seller = sellerDao.findById(3);
+		seller.setName("MARCAO");
+		sellerDao.update(seller);
+		System.out.println("Update no seller.");
+
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		
+		System.out.println("\n=== Teste 6 : seller delete ===");
+		System.out.println("Digite o codigo do Seller para deleção : ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id); 
+		System.out.println("Delete complete.");
+
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -64,14 +64,48 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			String sql = "UPDATE seller "
+					   + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					   + "WHERE Id = ? ";
+			
+			st = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartament().getId());
+			st.setInt(6, obj.getId());
+
+			st.executeUpdate();
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			String sql = "DELETE FROM seller "
+					   + "WHERE Id = ? ";
+			
+			st = conn.prepareStatement(sql);
+			st.setInt(1, id);
+
+			st.executeUpdate();
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
